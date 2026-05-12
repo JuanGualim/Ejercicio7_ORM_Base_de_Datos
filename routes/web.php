3c43fdc44ff2ebb24d851884aa5db2ffd91143f7
JuanGualim <gua24852@uvg.edu.gt>
2026-05-12 00:44:48 -0600

    Agregación de rutas con consultas Eloquent

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+
+// 1
+Route::get('/orders', function () {
+    return Order::with(['user', 'items.product'])->get();
+});
+
+// 2
+Route::get('/products-categories', function () {
+    return Product::with('categories')->get();
+});
+
+// 3
+Route::get('/products-reviews', function () {
+    return Product::whereHas('reviews', function ($q) {
+        $q->where('rating', '>', 4);
+    })->with('reviews')->get();
+});
+
+// 4
+Route::get('/users-orders', function () {
+    return User::whereHas('orders', function ($q) {
+        $q->where('total', '>', 100);
+    })->with('orders')->get();
+});
+
+// 5
+Route::get('/products-price', function () {
+    return Product::orderBy('price', 'desc')->get();
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
